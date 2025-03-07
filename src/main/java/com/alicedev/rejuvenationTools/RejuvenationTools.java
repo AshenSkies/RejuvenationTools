fc7856c8a749f0e36d198e6c1301a45c64970358
N. Star <96796586+AshenSkies@users.noreply.github.com>
2025-03-07 06:21:15 -0300

    Add basic HTTP server logic

--- src/main/java/com/alicedev/rejuvenationTools/RejuvenationTools.java
+++ src/main/java/com/alicedev/rejuvenationTools/RejuvenationTools.java
@@ -1,13 +1,29 @@
 package com.alicedev.rejuvenationTools;
 
 import com.sun.net.httpserver.HttpServer;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.concurrent.Executors;
+
 public final class RejuvenationTools extends JavaPlugin {
+
+    private static HttpServer server;
+    private static RejuvenationTools instance;
 
     @Override
     public void onEnable() {
         getLogger().info("Initializing plugin...");
+
+        try {
+            server = HttpServer.create(new InetSocketAddress(7867), 0);
+            server.setExecutor(Executors.newCachedThreadPool());
+            server.start();
+        } catch (IOException e) {
+            disablePlugin("Could not open the port for the web server: " + e.getMessage());
+        }
 
         getLogger().info("Plugin successfully initialized.");
     }
@@ -15,5 +31,18 @@ public final class RejuvenationTools extends JavaPlugin {
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    public static void disablePlugin(String message) {
+        System.out.println(getPrefix()+message);
+        Bukkit.getPluginManager().disablePlugin(Bukkit.getPluginManager().getPlugin(getInstance().getName()));
+    }
+
+    public static String getPrefix() {
+        return "["+getInstance().getName()+"] ";
+    }
+
+    public static RejuvenationTools getInstance() {
+        return instance;
     }
 }
